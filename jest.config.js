1f14050c01dd9a76cee06181142e410f34802a93
XhmikosR <xhmikosr@gmail.com>
2023-04-18 08:47:52 +0300

    Move stuff out of package.json (#230)

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
