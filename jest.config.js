9fa45ea7b5bb1f5b915809fe798d1d2f5adfba83
XhmikosR <xhmikosr@gmail.com>
2026-05-13 07:46:52 +0300

    Switch to ESM

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
-'use strict';
-
-module.exports = {
+export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
   },
   testEnvironment: 'node',
-  testRegex: '(/src\/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/src/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
